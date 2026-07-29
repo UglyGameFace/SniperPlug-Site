683c8d60a8e2f029cef3fb5344c4ee33e4b04c17
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-07-28 21:37:27 -0400

    Remove obsolete discovery stylesheet loader

--- assets/js/whop-discovery.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  if (document.querySelector('link[href="/assets/css/whop-discovery.css"]')) return;
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = '/assets/css/whop-discovery.css';
-  document.head.append(link);
-})();
