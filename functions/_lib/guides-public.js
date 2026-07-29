bb54f65dcf64bf721f96151518c8f3760531c226
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-07-29 17:40:21 -0400

    Use unified importer cleanup for public guides

--- functions/_lib/guides-public.js
+++ functions/_lib/guides-public.js
@@ -1,12 +1,12 @@
-import { quarantineUnsafePublishedGuides } from './content-policy.js';
 import { publicGuide as readPublicGuide, publicGuides as readPublicGuides } from './guides.js';
+import { reconcileImportedGuides } from './import-reconciliation.js';
 
 export async function publicGuide(env, slug) {
-  await quarantineUnsafePublishedGuides(env);
+  await reconcileImportedGuides(env);
   return readPublicGuide(env, slug);
 }
 
 export async function publicGuides(env, input = {}) {
-  await quarantineUnsafePublishedGuides(env);
+  await reconcileImportedGuides(env);
   return readPublicGuides(env, input);
 }
